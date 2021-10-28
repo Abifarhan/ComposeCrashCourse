708faf31e11e42bac4512b21c855fd0e80a47cbc
abiFarhan <abifarhan04@gmail.com>
2021-10-28 17:29:48 +0700

    Learn Text -> Implement text with clickable

--- app/src/main/java/com/abifarhan/composecrashcourse/MainActivity.kt
+++ app/src/main/java/com/abifarhan/composecrashcourse/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.abifarhan.composecrashcourse
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -11,7 +14,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.abifarhan.composecrashcourse.ui.theme.ComposeCrashCourseTheme
@@ -19,7 +24,9 @@ import com.abifarhan.composecrashcourse.ui.theme.ComposeCrashCourseTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContent{
+            Title()
+        }
     }
 
 
@@ -27,14 +34,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Title() {
+    val context = LocalContext.current
     Text(
         text = "Abi Jetpack Compose", fontSize = 32.sp,
-        color = colorResource(id = R.color.purple_700)
+        fontFamily = FontFamily.Cursive,
+        color = colorResource(id = R.color.purple_700),
+        modifier = Modifier.clickable {
+            Toast.makeText(context, "Anda klik", Toast.LENGTH_SHORT).show()
+        }
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Title()
+    androidx.compose.material.Surface(Modifier.fillMaxSize()) {
+        Title()
+    }
 }
