1eca90edeb18a51c0741bed922b062ef5b4596fd
abiFarhan <abifarhan04@gmail.com>
2021-11-04 11:53:06 +0700

    adding material and color in android design in text including padding

--- app/src/main/java/com/abifarhan/composecrashcourse/MainActivity.kt
+++ app/src/main/java/com/abifarhan/composecrashcourse/MainActivity.kt
@@ -66,8 +66,12 @@ fun MessageCard(msg: Message) {
 
 //            Add a vertical space between the author adnd message texts
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body,
-            style = MaterialTheme.typography.body2)
+
+            androidx.compose.material.Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2)
+            }
         }
     }
 
